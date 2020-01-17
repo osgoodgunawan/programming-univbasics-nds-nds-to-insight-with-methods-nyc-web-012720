fc1883a2aa1647d47f9cdb986f7f67db082d94eb
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-17 07:45:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,12 +15,6 @@ end
 
 def directors_totals(nds)
  result = {}
-  director_index = 0
-  while director_index < nds.size do
-    director = nds[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
+  
 end
 
